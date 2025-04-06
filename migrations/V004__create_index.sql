f95dd4b94b51c8a9c543c5eef82e34a0865ebb8a
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-04-06 19:44:31 +0300

    feat: change insert data

--- migrations/V004__create_index.sql
+++ migrations/V004__create_index.sql
@@ -3,6 +3,3 @@ CREATE INDEX order_product_order_id_idx ON order_product(order_id);
 
 -- Составной индекс для ускорения выборок заказов по статусу и дате создания
 CREATE INDEX orders_status_date_idx ON orders(status, date_created);
-
--- Индекс для ускорения поиска заказов по статусу
-CREATE INDEX orders_status_idx ON orders(status);
